12257db11563d0a27dd6ef6a1d740c28656b50ff
medisco <ismael@blockparty.io>
2025-10-04 14:52:15 -0300

    fix(prb-2): fix student management CRUD

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,40 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const getAllPayload = { 
+        name: req.query.name,
+        className: req.query.class,
+        section: req.query.section,
+        roll: req.query.roll
+    };
+    const students = await getAllStudents(getAllPayload);
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const student = await addNewStudent(req.body);
+    res.json({ student });
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const updatePayload = { userId: req.params.id, ...req.body };
+    const result = await updateStudent(updatePayload);
+    res.json(result);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const student = await getStudentDetail(req.params.id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const statusPayload = { 
+        userId: req.params.id, 
+        reviewerId: req.user.id, 
+        status: req.body.status
+    };
+    const student = await setStudentStatus(statusPayload);
+    res.json({ student });
 });
 
 module.exports = {
